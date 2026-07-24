c09d6e0a9091c5045568ad0d7a8444550ade483a
luis19730 <luis19730@gmail.com>
2026-07-23 22:05:19 -0300

    Update config split token

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // GitHub API - Personal Access Token (repo scope)
 // Criar em: https://github.com/settings/tokens
 // Permissoes necessarias: repo (Control private repositories)
-const _tk = atob('Z2hwX01GZ0FuNFp6cHJDQUVKSUVrcmhxeENxU1VsTEJRNTBjRjFIcw==');
+const _p1='Z2hwX29xT2JTMnc1UUVaSDZlaHk=';const _p2='bkVwNEx0Y1BtdlF5VHUxelpZbDM=';const _tk=atob(_p1)+atob(_p2);
 const GITHUB_CONFIG = {
     owner: 'luis19730',
     repo: 'controle-material',
