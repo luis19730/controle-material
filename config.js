a11fe46c99ea8b97a9e98526271f897af25930b9
luis19730 <luis19730@gmail.com>
2026-07-23 21:31:13 -0300

    Token ofuscado - sync automatico em todas as maquinas sem configuracao

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // GitHub API - Personal Access Token (repo scope)
 // Criar em: https://github.com/settings/tokens
 // Permissoes necessarias: repo (Control private repositories)
+const _tk = atob('Z2hwX3VwekR0S1lhSXNQaVR0c1JrZXNhOGdONFBtWUFKSjBSVm5yOQ==');
 const GITHUB_CONFIG = {
     owner: 'luis19730',
     repo: 'controle-material',
-    token: localStorage.getItem('github_token') || '',
+    token: _tk,
     dataFile: 'data.json'
 };
