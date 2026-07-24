1a39fda1d39bdb200ed5c6c2bdfe836d3d2565fe
luis19730 <luis19730@gmail.com>
2026-07-23 21:56:21 -0300

    Update config.js via API

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // GitHub API - Personal Access Token (repo scope)
 // Criar em: https://github.com/settings/tokens
 // Permissoes necessarias: repo (Control private repositories)
-const _tk = atob('Z2hwX24zU1lEakNCV3g3S0R4TUlUOU1nY2NTQkFFMWxsajBHUlk2Yg==');
+const _tk = atob('Z2hwX01GZ0FuNFp6cHJDQUVKSUVrcmhxeENxU1VsTEJRNTBjRjFIcw==');
 const GITHUB_CONFIG = {
     owner: 'luis19730',
     repo: 'controle-material',
